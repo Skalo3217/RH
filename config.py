b192676782f7235bbe3f944c6b9375c3254240d7
Skalo3217 <33033999+Skalo3217@users.noreply.github.com>
2021-02-06 16:57:29 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+## https://join.robinhood.com/robertc12673
+## btc wallet = 3PX5QLaT4gQnZLGpjaTYYk2RV32RDJmswK
+
 DATA_PATH = r'C:\Users\user\Desktop\RH\data' # Where csv file from robinhood will dump to
 RH_EMAIL = '************' #Robinhood username
 RH_PASS = '**************' #Robinhood password
